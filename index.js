4db8ed8ec5c6c7cc0a03e424466024b3923c5d7b
Ankur Kumar <74294349+Ankur42177@users.noreply.github.com>
2021-10-21 21:12:48 +0530

    setup of static files and setting up layouts and partial in the project

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express=require('express');
 const app= express();
 const port=7000;
+const expressLayout= require('express-ejs-layouts');
+app.use(expressLayout);
+app.use(express.static('./assets'));
 //use express router 
 app.use('/',require('./routes/index'));
 //setup a view engine 
 app.set('view engine','ejs');
 app.set('views','./views');
+//extract styles and sub-pages into the layouts
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 
 
 app.listen(port,function(err){
