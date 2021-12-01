e29505ce992ff66d79c36045ce868685826d4cd2
Ankur Kumar <74294349+Ankur42177@users.noreply.github.com>
2021-12-01 12:17:14 +0530

    all mistakes corrected

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,14 @@
+const Post=require('../models/post');
 module.exports.home=function(req,res){
-    console.log(req.cookies);
-    res.cookie('ank',909);//changing cookie at a server side in response which goes to browser
+    // console.log(req.cookies);
+    // res.cookie('ank',909);//changing cookie at a server side in response which goes to browser
    
-    return res.render('home',{title:"ankurmessagebook"});
+    // return res.render('home',{title:"ankurmessagebook"});
+    Post.find({},function(err,post){
+        return res.render('home',{
+            title:"MessageBook|Home",
+            post:post
+        });
+
+    });
 }
